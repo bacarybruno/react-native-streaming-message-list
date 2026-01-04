6a1623fddbe38d4c9573a6d7f9158ac6e6faf3a1
bacarybruno <brunobacary.bodian@doctolib.com>
2026-01-04 20:16:54 +0100

    fix: update default variant to 'chatgpt' in App.tsx

--- examples/App.tsx
+++ examples/App.tsx
@@ -3,7 +3,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { BasicChatScreen } from './src/basic/screen';
 import { ChatGPTScreen } from './src/chatgpt/screen';
 
-const variant = (process.env.EXPO_PUBLIC_DEMO_VARIANT || 'basic') as
+const variant = (process.env.EXPO_PUBLIC_DEMO_VARIANT || 'chatgpt') as
   | 'basic'
   | 'chatgpt';
 
